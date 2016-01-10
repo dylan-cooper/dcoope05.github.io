d8916090927c92d3e28dd98709caea1d99c9374c
Dylan Cooper <dcoope05@mail.uoguelph.ca>
2016-01-10 00:45:11 -0500

    Finished up the F15 updates

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
  * Dylan Cooper
  */
 
-var errorPage = 'posts/yikes.html';
-var homePage = 'posts/home.html';
+var errorPage = {path: 'posts/yikes.html', title: 'Yikes!'};
+var homePage = {path: 'posts/home.html', title: 'Home'};
 
 var map = {
     "" : homePage,
-    "about-me" : 'posts/about-me.html',
-    "about-site": 'posts/about-site.html',
+    "about-me" : {path: 'posts/about-me.html', title: 'About Me'},
+    "about-site": {path: 'posts/about-site.html', title: 'About This Site'},
     "coop": {
-        "" : 'posts/coop/home.html',
-        "ccs" : 'posts/coop/ccs.html',
-        "freshbooks" : 'posts/coop/freshbooks.html'
+        "" : {path: 'posts/coop/home.html', title: 'Co-ops'},
+        "ccs" : {path: 'posts/coop/ccs.html', title: 'Co-op at CCS'},
+        "freshbooks" : {path: 'posts/coop/freshbooks.html', title: 'Co-op at FreshBooks'}
     },
 }
 
@@ -27,26 +27,23 @@ $(window).on('hashchange', function(){
 
 function render(hash){
     var parts = hash.split('/');
-
+    
+    parts[2] = (parts[2] === undefined) ? '' : parts[2];
 
     if (hash == '' || hash == '#'){
         result = homePage;
     } else if (parts.length > 3 || parts[0] != '#' || map[parts[1]] === undefined){
         //Error in the hash
         result = errorPage;
-    } else if (typeof map[parts[1]] == 'string'){
+    } else if (typeof map[parts[1]].path == 'string'){
         result = map[parts[1]];
-    } else if (typeof map[parts[1]] == 'object'){
-        parts[2] = (parts[2] === undefined) ? '' : parts[2];
+    } else if (typeof map[parts[1]][parts[2]].path == 'string'){
         result = map[parts[1]][parts[2]];
     } else {
         result = errorPage;
     }
 
     console.log(result);
-    $('#content').load(result);
-}
-
-function renderHome(){
-    $('#content').load('posts/home.html');
+    $('#content').load(result.path);
+    document.title = 'Dylan Cooper | ' + result.title;
 }
